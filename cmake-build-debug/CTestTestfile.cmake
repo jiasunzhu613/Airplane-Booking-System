387195820a46b7bcab7bae2308da01da3a0b8999
jonathanzhu <jiasunzhu@gmail.com>
2023-06-07 00:05:07 -0400

    Fifth commit

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jonathanzhu/CLionProjects/Airplane_Booking_System
-# Build directory: /Users/jonathanzhu/CLionProjects/Airplane_Booking_System/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
